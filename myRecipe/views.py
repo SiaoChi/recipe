3ac94573f297ed316fd8e947496a718ecaf37e9e
Kelly <chichigou@gmail.com>
2022-04-24 01:23:45 +0800

    cache_page

--- myRecipe/views.py
+++ myRecipe/views.py
@@ -4,6 +4,8 @@ from .models import Recipe, Material ,Sauce ,RecipeImage, User, Tag
 from Recipe.form import RecipeForm, MaterialFormSet, SauceFormSet
 from django.contrib import messages
 from .utils import searchRecipe, searchMyRecipe
+from django.views.decorators.cache import cache_page
+
 
 
 
@@ -11,6 +13,7 @@ from .utils import searchRecipe, searchMyRecipe
 # Create your views here.
 
 #首頁 食譜總覽
+@cache_page(60 * 15)
 def Recipes(request):
 
      recipes, search_query = searchRecipe(request)
@@ -23,6 +26,7 @@ def Recipes(request):
 
 
 #瀏覽單一食譜
+@cache_page(60 * 15)
 def singleRecipe(request,pk):
     recipe = Recipe.objects.get(id=pk)
     tags = recipe.tags.all()
