ca7d0c1797060f1cf123dd81f5d19300f5789c57
Kelly <chichigou@gmail.com>
2022-05-01 17:23:26 +0800

    pagination

--- myRecipe/models.py
+++ myRecipe/models.py
@@ -23,10 +23,11 @@ class Recipe(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-    def save(self, *args, **kwargs):
-        instance = super(Recipe, self).save(*args, **kwargs)
-        img = Image.open(instance.featured_image.path)
-        img.save(instance.featured_image.path, quality=20, optimize=True)
+    # def save(self, *args, **kwargs):
+    #     instance = super(Recipe, self).save(*args, **kwargs)
+    #     img = Image.open(instance.featured_image.path)
+    #     img.save(instance.featured_image.path, quality=20, optimize=True)
+    #     return instance
 
     def __str__(self):
         return self.name
