0dc000a1d3c4326f097d918cc1390992d2ef0590
Kelly <chichigou@gmail.com>
2022-05-02 15:38:59 +0800

    pillow compress

--- myRecipe/models.py
+++ myRecipe/models.py
@@ -7,10 +7,19 @@ from PIL import Image
 import os
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 
 
 # Create your models here.
+
+def compress(featured_image):
+    im = Image.open(featured_image)
+    im_io = BytesIO()
+    im.save(im_io, 'WebP', quality=50, optimize =True)
+    new_image = File(im_io, name=featured_image.name)
+    return new_image
 
 class Recipe(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
@@ -22,6 +31,13 @@ class Recipe(models.Model):
     source_link = models.URLField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    # calling image compression function before saving the data
+    def save(self, *args, **kwargs):
+        new_image = compress(self.featured_image)
+        self.featured_image = new_image
+        super().save(*args, **kwargs)
+        print ('compressed!')
 
     # def save(self, *args, **kwargs):
     #     instance = super(Recipe, self).save(*args, **kwargs)
