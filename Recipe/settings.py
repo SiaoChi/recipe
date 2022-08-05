b17e895e40297081daa3a382da986ea228b100d8
Kelly <chichigou@gmail.com>
2022-08-05 16:36:06 +0800

    email fixed2

--- Recipe/settings.py
+++ Recipe/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
 EMAIL_HOST_USER = 'chichigou.work@gmail.com'
-EMAIL_HOST_PASSWORD = 'RamoKelly1989@'
+EMAIL_HOST_PASSWORD = 'krobyrgowlymrlkm'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
